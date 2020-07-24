83e9138830a72f8d1ebc026631f36cc8584ed960
Maaack <marek.belski@gmail.com>
2020-07-23 18:00:50 -0700

    INIT-01: Deck icons hug correct corners.

--- Scenes/HUD/HUD.gd
+++ Scenes/HUD/HUD.gd
@@ -1,12 +1,15 @@
 extends Control
 
 
+onready var draw_pile = $BottomLeft/DrawPile
+onready var hand = $CenterBottom/Hand
+
 var starter_deck = preload("res://Resources/DeckSettings/StartingDeck.tres")
 
 func _ready():
 	randomize()
-	$DrawPile.set_deck_settings(starter_deck)
-	$DrawPile.shuffle()
-	var cards : Array = $DrawPile.draw_hand(5)
+	draw_pile.set_deck_settings(starter_deck)
+	draw_pile.shuffle()
+	var cards : Array = draw_pile.draw_hand(5)
 	for card_scene in cards:
-		$CenterBottom/Hand.add_card(card_scene)
+		hand.add_card(card_scene)
