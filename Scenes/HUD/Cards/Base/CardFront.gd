b1fe87a7a7f54e0fb94b63a6e3be00688969f90b
Maaack <marek.belski@gmail.com>
2020-07-28 12:55:15 -0700

    BATL-01: Cards also glow when hovered over.

--- Scenes/HUD/Cards/Base/CardFront.gd
+++ Scenes/HUD/Cards/Base/CardFront.gd
@@ -2,7 +2,17 @@ tool
 extends Node2D
 
 
+signal glow_on
+signal glow_off
+
+onready var glow_node = $Control/CenterContainer/Control/GlowNode
+onready var animation_node = $Control/CenterContainer/Control/GlowNode/AnimationPlayer
+
 export(Resource) var card_settings setget set_card_settings
+
+var hovering : bool = false
+var pressed : bool = false
+var glowing : bool = false
 
 
 func set_card_settings(value:CardSettings):
@@ -16,8 +26,33 @@ func _update_card_front():
 	if not is_instance_valid(card_settings):
 		return
 	if card_settings.title != "":
-		$Control/TitlePanel/TitleLabel.text = card_settings.title
+		$Control/CardFront/TitlePanel/TitleLabel.text = card_settings.title
 	if card_settings.description != "":
-		$Control/DescriptionPanel/MarginContainer/DescriptionLabel.text = card_settings.description
+		$Control/CardFront/DescriptionPanel/MarginContainer/DescriptionLabel.text = card_settings.description
 	if card_settings.bde_cost >= 0:
 		$Control/BDEPanel/BDECostLabel.text = str(card_settings.bde_cost)
+
+func _update_glow():
+	if hovering and not pressed:
+		if not glowing:
+			glowing = true
+			emit_signal("glow_on", self)
+		glow_node.show()
+		animation_node.play("Glow")
+	else:
+		if glowing:
+			glowing = false
+			emit_signal("glow_off", self)
+		glow_node.hide()
+		animation_node.stop()
+
+func _on_CardFront_mouse_entered():
+	hovering = true
+	_update_glow()
+
+func _on_CardFront_mouse_exited():
+	hovering = false
+	_update_glow()
+
+func _on_CardFront_gui_input(event):
+	pass # Replace with function body.
