ab9781ff9669448967438bc243dcdc4fc78983d4
Maaack <marek.belski@gmail.com>
2020-07-27 18:41:12 -0700

    BATL-01: Timer adds delay before drawing next hand.

--- Scenes/HUD/HUD.gd
+++ Scenes/HUD/HUD.gd
@@ -4,6 +4,7 @@ extends Control
 onready var draw_pile = $DrawPile
 onready var discard_pile = $DiscardPile
 onready var hand = $CenterBottom/Hand
+onready var timer = $Timer
 
 var starter_deck = preload("res://Resources/DeckSettings/StartingDeck.tres")
 
@@ -11,10 +12,14 @@ func _ready():
 	randomize()
 	draw_pile.set_deck_settings(starter_deck)
 	draw_pile.shuffle()
-	var cards : Array = draw_pile.draw_hand(5)
-	for card_scene in cards:
-		hand.add_card(card_scene)
+	timer.start()
 
 func _on_EndTurnButton_pressed():
 	var discarded_cards : Array = hand.discard_hand()
 	discard_pile.add_cards(discarded_cards)
+	timer.start()
+
+func _on_Timer_timeout():
+	var cards : Array = draw_pile.draw_hand(5)
+	for card_scene in cards:
+		hand.add_card(card_scene)
