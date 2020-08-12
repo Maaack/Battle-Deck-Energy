18e25396045b6ff8bf5032789cb5f8b9654d1f9a
Maaack <marek.belski@gmail.com>
2020-08-12 12:18:41 -0700

    RFCT-01: Separating out display of deck counts from real data.

---
+++ Scenes/HUD/CardManager/BattleBoard/PlayerBoard/PlayerBoard.gd
@@ -0,0 +1,49 @@
+extends Control
+
+
+signal ending_turn
+
+onready var draw_pile = $DrawPile
+onready var discard_pile = $DiscardPile
+onready var exhaust_pile = $ExhaustPile
+onready var end_turn_button = $EndTurnButton
+onready var round_counter = $RoundCounter
+onready var energy_meter = $BattleDeckEnergy
+
+func _ready():
+	reset_end_turn()
+	set_draw_pile_size(6)
+
+func set_draw_pile_size(value:int):
+	draw_pile.count = value
+
+func set_player_energy(max_energy:int, energy:int=-1):
+	energy_meter.max_energy = max_energy
+	if energy > 0:
+		energy_meter.energy = energy
+	else:
+		energy_meter.energy = max_energy
+
+func draw_card():
+	draw_pile.remove_card()
+
+func draw_discarded_card():
+	discard_pile.remove_card()
+
+func reshuffle_card():
+	draw_pile.add_card()
+
+func discard_card():
+	discard_pile.add_card()
+
+func exhaust_card():
+	exhaust_pile.add_card()
+
+func signal_ending_turn():
+	emit_signal("ending_turn")
+
+func _on_EndTurnButton_pressed():
+	signal_ending_turn()
+
+func reset_end_turn():
+	end_turn_button.reset()
