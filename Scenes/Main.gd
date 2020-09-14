a135f100f8b93051064c2689c6b394ff98c5a0e9
Maaack <marek.belski@gmail.com>
2020-09-14 15:33:18 -0700

    LOOT-01: Separates out shadow panel from victory and defeat panels.

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -5,6 +5,7 @@ onready var battle_interface_container = $BattleInterfaceContainer
 onready var battle_interface = $BattleInterfaceContainer/BattleInterface
 onready var dead_panel = $DeadPanel
 onready var victory_panel = $VictoryPanel
+onready var shadow_panel = $ShadowPanel
 onready var level_manager = $LevelManager
 
 var starting_player_data : CharacterData = preload("res://Resources/Characters/Player/NewPlayerData.tres")
@@ -26,19 +27,23 @@ func _ready():
 	start_battle()
 
 func _on_DeadPanel_retry_pressed():
+	shadow_panel.hide()
 	dead_panel.hide()
 	player_data = starting_player_data.duplicate()
 	start_battle()
 
 func _on_BattleInterface_player_lost():
 	battle_interface.queue_free()
+	shadow_panel.show()
 	dead_panel.show()
 
 func _on_BattleInterface_player_won():
 	battle_interface.queue_free()
+	shadow_panel.show()
 	victory_panel.show()
 
 func _on_VictoryPanel_continue_pressed():
+	shadow_panel.hide()
 	victory_panel.hide()
 	level_manager.advance()
 	start_battle()
