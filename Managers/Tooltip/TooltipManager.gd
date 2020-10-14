b6a42021ba4e6eb46a28db55a24c11dcb92ada92
Maaack <marek.belski@gmail.com>
2020-10-14 15:09:40 -0700

    ART-01: Updates tooltips and main theme.

--- Managers/Tooltip/TooltipManager.gd
+++ Managers/Tooltip/TooltipManager.gd
@@ -31,19 +31,24 @@ func define_key(key:String, list_instance:TooltipListNode2D):
 	var definition_data : DefinitionData = key_definition_map[key]
 	list_instance.add_definition_tooltip(definition_data)
 
-func show_definitions(keys:Array, list_position:Vector2):
+func show_definitions(keys:Array, list_position:Vector2, list_upward:bool = false):
 	_reset_tooltips()
 	var tooltip_list_instance : TooltipListNode2D = tooltip_list_scene.instance()
 	tooltip_list_instance.position = list_position
 	add_child(tooltip_list_instance)
+	if list_upward:
+		tooltip_list_instance.tooltip_container.grow_vertical = Control.GROW_DIRECTION_BEGIN
+		tooltip_list_instance.tooltip_container.rect_position.y -= tooltip_list_instance.tooltip_container.rect_size.y
 	for key in keys:
 		define_key(key, tooltip_list_instance)
 
 func inspect_card(card_node:CardNode2D):
 	var keys : Array = []
 	var half_width : float = get_rect().size.x / 2.0
+	var half_height : float = get_rect().size.y / 2.0
 	var card_position : Vector2 = card_node.get_global_transform().get_origin()
 	var list_position : Vector2 = card_node.right_tooltip_target.global_position
+	var list_upward : bool = false
 	if card_position.x > half_width:
 		list_position = card_node.left_tooltip_target.global_position
 	for effect_data in card_node.card_data.effects:
@@ -53,7 +58,12 @@ func inspect_card(card_node:CardNode2D):
 					keys.append(status_data.type_tag)
 		elif effect_data is EffectData:
 			keys.append(effect_data.type_tag)
-	show_definitions(keys, list_position)
+	if card_position.y > half_height and keys.size() > 1:
+		list_upward = true
+		list_position = card_node.bottom_right_tooltip_target.global_position
+		if card_position.x > half_width:
+			list_position = card_node.bottom_left_tooltip_target.global_position
+	show_definitions(keys, list_position, list_upward)
 
 func inspect_status(status_icon:StatusIcon):
 	var list_position : Vector2 = status_icon.tooltip_target.global_position
--- Scenes/TooltipList/DefinitionTooltip/DefinitionTooltip.gd
+++ Scenes/TooltipList/DefinitionTooltip/DefinitionTooltip.gd
@@ -1,6 +1,9 @@
 extends PanelContainer
 
 
+const TERM_BBCODE_STRING = "[center][b]%s[/b][/center]"
+const DEFINITION_BBCODE_STRING = "[center]%s[/center]"
+
 onready var term_label = $MarginContainer/VBoxContainer/TermLabel
 onready var definition_label = $MarginContainer/VBoxContainer/DefinitionLabel
 
@@ -8,5 +11,5 @@ var term : String = ""
 var definition : String = ""
 
 func _ready():
-	term_label.text = term
-	definition_label.bbcode_text = "[center]%s[/center]" % definition
+	term_label.bbcode_text = TERM_BBCODE_STRING % term
+	definition_label.bbcode_text = DEFINITION_BBCODE_STRING % definition
