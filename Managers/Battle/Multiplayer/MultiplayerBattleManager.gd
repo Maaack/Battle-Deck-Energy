ce72fb0021e053bb0efa53559f33af291c7573c9
Maaack <marek.belski@gmail.com>
2020-11-18 17:03:08 -0800

    MULTI-01: Fixes unmarking of inactive players in multiplayer.

--- Managers/Battle/Multiplayer/MultiplayerBattleManager.gd
+++ Managers/Battle/Multiplayer/MultiplayerBattleManager.gd
@@ -15,11 +15,17 @@ remotesync func _remote_on_card_played(card_key : String, opportunity_source : S
 	var card = card_library.data[card_key]
 	effects_manager.resolve_on_play_opportunity(card, opportunity, _character_manager_map)
 
+remotesync func _remote_on_turn_ended(character_nickname):
+	for character in _character_manager_map:
+		if character.nickname == character_nickname:
+			emit_signal("turn_ended", character)
+
 func _on_CharacterBattleManager_card_played(character : CharacterData, card:CardData, opportunity:OpportunityData):
 	rpc('_remote_on_card_played', card.title, opportunity.source.nickname, opportunity.target.nickname, opportunity.type)
 	opportunities_manager.remove_opportunity(opportunity)
 	_discard_or_exhaust_card(character, card)
 
 func _on_CharacterBattleManager_turn_ended(character : CharacterData):
+	rpc('_remote_on_turn_ended', character.nickname)
 	if character == active_character:
 		rpc('advance_character_phase')
