6bfc3f2b8cbd6786e5ff65cdc1f27a86d7168981
Maaack <marek.belski@gmail.com>
2020-12-01 22:16:17 -0800

    EFFCT-01: Regenerates card weights and card lists. All level loot temporarily reset.

--- Resources/Levels/BattleLevelData.gd
+++ Resources/Levels/BattleLevelData.gd
@@ -4,7 +4,7 @@ extends LevelData
 class_name BattleLevelData
 
 export(Array, Resource) var opponents : Array = []
-export(Resource) var lootable_cards : Resource = preload("res://Resources/Weighted/WeightedCardList/Rank1CardList.tres") setget set_lootable_cards
+export(Resource) var lootable_cards : Resource = preload("res://Resources/Weighted/WeightedCardList/DefaultCardList.tres") setget set_lootable_cards
 
 func set_lootable_cards(value:WeightedDataList):
 	lootable_cards = value
