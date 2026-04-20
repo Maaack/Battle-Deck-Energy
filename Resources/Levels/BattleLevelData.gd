2fcf866c96d1e365ed0b83b702a31adc6926d560
Marek Belski <marek.belski@gmail.com>
2026-04-19 19:59:00 -0700

    Fix battle level data preload.

--- Resources/Levels/BattleLevelData.gd
+++ Resources/Levels/BattleLevelData.gd
@@ -4,7 +4,7 @@ extends LevelData
 class_name BattleLevelData
 
 @export var opponents : Array = [] # (Array, Resource)
-@export var lootable_cards: Resource = preload("res://Resources/Weighted/WeightedCardList/DefaultCardList.tres"): set = set_lootable_cards
+@export var lootable_cards: Resource : set = set_lootable_cards
 
 func set_lootable_cards(value:WeightedDataList):
 	lootable_cards = value
