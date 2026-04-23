7755f4ce66d7107e634f909d39118fedf9630aaf
Marek Belski <marek.belski@gmail.com>
2026-04-23 09:59:16 -0700

    Updeates debug loot panel to print 1000 samples of loot by level.

--- Scenes/LootPanel/debug_loot_panel.gd
+++ Scenes/LootPanel/debug_loot_panel.gd
@@ -1,18 +1,36 @@
 extends "res://Scenes/LootPanel/LootPanel.gd"
 
+const MAX_GENERATIONS : int = 1000
+const MAX_TICK_GENERATIONS : int = 30
 
 var card_count_map : Dictionary[String, int]
 var generations : int = 0
+var tick_generations : int = 0
+
+@export var all_levels : Array[BattleLevelData]
+
+var current_level_iter : int = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	_generate_card_options()
-	generations += 1
-	for card in card_options:
-		if card.title not in card_count_map:
-			card_count_map[card.title] = 1
-			continue
-		card_count_map[card.title] += 1
-
-func _on_timer_timeout():
-	print(generations, " : ", card_count_map)
+	while tick_generations < MAX_TICK_GENERATIONS:
+		if current_level_iter >= all_levels.size():
+			print("finished")
+			set_process(false)
+			return
+		battle_level = all_levels[current_level_iter]
+		_generate_card_options()
+		for card in card_options:
+			if card.title not in card_count_map:
+				card_count_map[card.title] = 1
+				continue
+			card_count_map[card.title] += 1
+		generations += 1
+		if generations >= MAX_GENERATIONS:
+			print(battle_level.resource_path.get_file().get_basename(), " : ", card_count_map)
+			print("")
+			card_count_map.clear()
+			generations = 0
+			current_level_iter += 1
+		tick_generations += 1
+	tick_generations = 0
