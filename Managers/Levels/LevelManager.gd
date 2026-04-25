26f6cb6f7c94df99100712c424d42eeb847501f5
Marek Belski <marek.belski@gmail.com>
2026-04-25 13:39:14 -0700

    Removes deprecated preloaded levels.

--- Managers/Levels/LevelManager.gd
+++ Managers/Levels/LevelManager.gd
@@ -1,8 +1,6 @@
 @tool
 extends Node
 
-
-@export var levels : Array = [] # (Array, Resource)
 @export var paths : Array[String]
 @export var starting_level: int = 0
 
@@ -10,10 +8,6 @@ var current_level : int = 0
 
 func reset():
 	current_level = starting_level
-	paths.clear()
-	for level in levels:
-		if level is Resource:
-			paths.append(level.resource_path)
 
 func advance():
 	current_level += 1
@@ -21,6 +15,3 @@ func advance():
 func get_current_level():
 	var level_index = current_level % paths.size()
 	return load(paths[level_index])
-
-func _ready():
-	reset()
