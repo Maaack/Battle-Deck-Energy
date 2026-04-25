3d77866e7b5b6f28dd05278a070ae05e12646dac
Marek Belski <marek.belski@gmail.com>
2026-04-25 13:37:44 -0700

    Switch levels from preloaded packed scenes to paths.

--- Managers/Levels/LevelManager.gd
+++ Managers/Levels/LevelManager.gd
@@ -1,23 +1,26 @@
+@tool
 extends Node
 
 
 @export var levels : Array = [] # (Array, Resource)
+@export var paths : Array[String]
 @export var starting_level: int = 0
 
 var current_level : int = 0
 
 func reset():
 	current_level = starting_level
+	paths.clear()
+	for level in levels:
+		if level is Resource:
+			paths.append(level.resource_path)
 
 func advance():
 	current_level += 1
 
 func get_current_level():
-	var level_index = current_level % levels.size()
-	return levels[level_index]
+	var level_index = current_level % paths.size()
+	return load(paths[level_index])
 
-func get_level_opponents():
-	var level : BattleLevelData = get_current_level()
-	if level == null:
-		return
-	return level.opponents
+func _ready():
+	reset()
