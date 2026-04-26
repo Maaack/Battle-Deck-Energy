1fb6d1e33bc3e5f83f8705842d76276ec1a7b037
Marek Belski <marek.belski@gmail.com>
2026-04-26 15:41:58 -0700

    Add difficulty to enemies. Add lowest difficulty enemies.

--- Resources/Characters/OpponentCharacterData.gd
+++ Resources/Characters/OpponentCharacterData.gd
@@ -3,3 +3,11 @@ extends CharacterData
 
 class_name OpponentCharacterData
 
+@export_range(1, 32) var difficulty : int = 1
+@export_range(0, 20) var starting_health_range : int = 0
+
+func reset_health() -> void:
+	super()
+	if starting_health_range > 0:
+		max_health += randi() % (starting_health_range + 1)
+		health = max_health
