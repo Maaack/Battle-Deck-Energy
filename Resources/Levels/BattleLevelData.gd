019551bd6acc255a77020485958cec1ba2a2de13
Marek Belski <marek.belski@gmail.com>
2026-04-27 10:55:21 -0700

    Moves converting difficulty to rank logic to battle level data.

--- Resources/Levels/BattleLevelData.gd
+++ Resources/Levels/BattleLevelData.gd
@@ -9,6 +9,35 @@ enum LootType{
 	TOXIC
 }
 
-@export var opponents : Array[CharacterData] = [] # (Array, Resource)
+@export var opponents : Array[CharacterData] : set = set_opponents
 @export var loot_type : LootType
 @export_range(1, 15) var rank : int = 1
+
+func _set_rank_from_difficulty(difficulty:int):
+	var _rank = difficulty - 1
+	if _rank < 1:
+		_rank = 1
+	if _rank > 6:
+		_rank = 6
+	rank = _rank
+	
+func set_opponents(value):
+	opponents = value
+	if opponents == null:
+		return
+	var highest_type_count := 0
+	var highest_type := OpponentCharacterData.OpponentType.UNKNOWN
+	var total_difficulty := 0
+	var type_count_map : Dictionary[OpponentCharacterData.OpponentType, int] = {
+		OpponentCharacterData.OpponentType.GRAPPLE : 0,
+		OpponentCharacterData.OpponentType.ROGUE : 0,
+		OpponentCharacterData.OpponentType.TOXIC : 0,
+	}
+	for opponent in opponents:
+		if opponent is OpponentCharacterData:
+			total_difficulty += opponent.difficulty
+			type_count_map[opponent.type] += 1
+			if highest_type != opponent.type and type_count_map[opponent.type] > highest_type_count:
+				highest_type = opponent.type
+	loot_type = (highest_type as BattleLevelData.LootType)
+	_set_rank_from_difficulty(total_difficulty)
